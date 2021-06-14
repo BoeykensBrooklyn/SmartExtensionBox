22ea5af125d59cf8a011e73c3f17c6563023370f
BoeykensBrooklyn <Brooklyn.boeykens@student.howest.be>
2021-06-14 13:45:44 +0200

    Arduino Mega

--- Backend/ArduinoCode/ArduinoMega.ino
+++ Backend/ArduinoCode/ArduinoMega.ino
@@ -90,8 +90,8 @@ unsigned long previousMillis = 0;
 
 void setup()
 {
-    // put your setup code here, to run once:
-    //Het aanmaken van input voor de drukknoppen
+// put your setup code here, to run once:
+//Het aanmaken van input voor de drukknoppen
 #pragma region Pinmode Inputs
     pinMode(IKnop1, INPUT_PULLUP);
     pinMode(Iknop2, INPUT_PULLUP);
@@ -103,7 +103,7 @@ void setup()
     pinMode(IKnop8, INPUT_PULLUP);
 #pragma endregion
 
-    //Het aanmaken van de output voor de uitgangen
+//Het aanmaken van de output voor de uitgangen
 #pragma region Pinmode Outputs
     pinMode(QUitgang1, OUTPUT);
     pinMode(QUitgang2, OUTPUT);
@@ -135,9 +135,8 @@ void setup()
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
-    RunningStatistics inputStats;
-    inputStats.setWindowSecs(windowLength);
+// put your main code here, to run repeatedly:
+
 // --------Drukknoppen Inlezen----------
 #pragma region drukknop
     // Drukknop 1 Toggle gedeelte
@@ -271,171 +270,167 @@ void loop()
 #pragma region Current Sensor
         if (incomingString.substring(0, 8) == "Verbruik")
         {
+            RunningStatistics inputStats;
+            inputStats.setWindowSecs(windowLength);
 
             ACS_Value = analogRead(ACS_Pin);
             inputStats.input(ACS_Value);
+            Amps_TRMS = intercept + slope * inputStats.sigma();
 
-            if ((unsigned long)(millis() - previousMillis) >= printPeriod)
-            {
-                previousMillis = millis();
-
-                Amps_TRMS = -0.10 + 0.07344 * inputStats.sigma();
-
-                // Watt => P = U . I
-                // Voltage => 230V
-                verbruik = 230 * Amps_TRMS;
-                Serial1.println("Verbruik:" + String(verbruik));
-                Serial.println("Amps:" + String(Amps_TRMS));
-                Serial.println("Verbruik:" + String(verbruik));
-            }
-        }
-#pragma endregion
-
-        // Schakelen van uitgang1
-#pragma region Schakel Uitgang1
-        if (incomingString.substring(0, 5) == "Knop1")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState1 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState1 = LOW;
-            }
-            Serial1.println("Ok");
-            Serial.println("Ok");
-        }
-#pragma endregion
-
-        // Schakelen van uitgang2
-#pragma region Schakel Uitgang2
-        if (incomingString.substring(0, 5) == "Knop2")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState2 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState2 = LOW;
-            }
-            Serial1.println("Ok");
-        }
-#pragma endregion
-
-        // Schakelen van uitgang3
-#pragma region Schakel Uitgang3
-        if (incomingString.substring(0, 5) == "Knop3")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState3 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState3 = LOW;
-            }
-            Serial1.println("Ok");
-        }
-#pragma endregion
-
-        // Schakelen van uitgang4
-#pragma region Schakel Uitgang4
-        if (incomingString.substring(0, 5) == "Knop4")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState4 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState4 = LOW;
-            }
-            Serial1.println("Ok");
-        }
-#pragma endregion
-
-        // Schakelen van uitgang5
-#pragma region Schakel Uitgang5
-        if (incomingString.substring(0, 5) == "Knop5")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState5 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState5 = LOW;
-            }
-            Serial1.println("Ok");
-        }
-#pragma endregion
-
-        // Schakelen van uitgang6
-#pragma region Schakel Uitgang6
-        if (incomingString.substring(0, 5) == "Knop6")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState6 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState6 = LOW;
-            }
-            Serial1.println("Ok");
-        }
-#pragma endregion
-
-        // Schakelen van uitgang7
-#pragma region Schakel Uitgang7
-        if (incomingString.substring(0, 5) == "Knop7")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState7 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState7 = LOW;
-            }
-            Serial1.println("Ok");
-        }
-#pragma endregion
-
-        // Schakelen van uitgang8
-#pragma region Schakel Uitgang8
-        if (incomingString.substring(0, 5) == "Knop8")
-        {
-            if (incomingString.substring(5, 9) == "Aan")
-            {
-                QState8 = HIGH;
-            }
-            else if (incomingString.substring(5, 9) == "Uit")
-            {
-                QState8 = LOW;
-            }
-            Serial1.println("Ok");
-        }
-#pragma endregion
-
-        //Informatie geven van de Waardes:
-#pragma region Stuur String met waardes terug
-        if (incomingString.substring(0, 5) == "Value")
-        {
-            Serial1.println("Values:Knop1:" + String(QState1) + "Knop2:" + String(QState2) + "Knop3:" + String(QState3) + "Knop4:" + String(QState4) + "Knop5:" + String(QState5) + "Knop6:" + String(QState6) + "Knop7:" + String(QState7) + "Knop8:" + String(QState8));
-        }
-#pragma endregion
-
-        // Test Seriële communicatie
-#pragma region teststring
-
-        if (incomingString == "Test")
-        {
-            Serial1.println("test geslaagd");
-            Serial.println("Test geslaagd");
+            // Watt => P = U . I
+            // Voltage => 230V
+            verbruik = 230 * Amps_TRMS;
+            Serial1.println("Verbruik:" + String(verbruik));
+            Serial.println("Amps:" + String(Amps_TRMS));
+            Serial.println("Verbruik:" + String(verbruik));
         }
     }
 #pragma endregion
+
+// Schakelen van uitgang1
+#pragma region Schakel Uitgang1
+    if (incomingString.substring(0, 5) == "Knop1")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState1 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState1 = LOW;
+        }
+        Serial1.println("Ok");
+        Serial.println("Ok");
+    }
+#pragma endregion
+
+// Schakelen van uitgang2
+#pragma region Schakel Uitgang2
+    if (incomingString.substring(0, 5) == "Knop2")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState2 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState2 = LOW;
+        }
+        Serial1.println("Ok");
+    }
+#pragma endregion
+
+// Schakelen van uitgang3
+#pragma region Schakel Uitgang3
+    if (incomingString.substring(0, 5) == "Knop3")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState3 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState3 = LOW;
+        }
+        Serial1.println("Ok");
+    }
+#pragma endregion
+
+// Schakelen van uitgang4
+#pragma region Schakel Uitgang4
+    if (incomingString.substring(0, 5) == "Knop4")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState4 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState4 = LOW;
+        }
+        Serial1.println("Ok");
+    }
+#pragma endregion
+
+// Schakelen van uitgang5
+#pragma region Schakel Uitgang5
+    if (incomingString.substring(0, 5) == "Knop5")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState5 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState5 = LOW;
+        }
+        Serial1.println("Ok");
+    }
+#pragma endregion
+
+// Schakelen van uitgang6
+#pragma region Schakel Uitgang6
+    if (incomingString.substring(0, 5) == "Knop6")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState6 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState6 = LOW;
+        }
+        Serial1.println("Ok");
+    }
+#pragma endregion
+
+// Schakelen van uitgang7
+#pragma region Schakel Uitgang7
+    if (incomingString.substring(0, 5) == "Knop7")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState7 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState7 = LOW;
+        }
+        Serial1.println("Ok");
+    }
+#pragma endregion
+
+// Schakelen van uitgang8
+#pragma region Schakel Uitgang8
+    if (incomingString.substring(0, 5) == "Knop8")
+    {
+        if (incomingString.substring(5, 9) == "Aan")
+        {
+            QState8 = HIGH;
+        }
+        else if (incomingString.substring(5, 9) == "Uit")
+        {
+            QState8 = LOW;
+        }
+        Serial1.println("Ok");
+    }
+#pragma endregion
+
+//Informatie geven van de Waardes:
+#pragma region Stuur String met waardes terug
+    if (incomingString.substring(0, 5) == "Value")
+    {
+        Serial1.println("Values:Knop1:" + String(QState1) + "Knop2:" + String(QState2) + "Knop3:" + String(QState3) + "Knop4:" + String(QState4) + "Knop5:" + String(QState5) + "Knop6:" + String(QState6) + "Knop7:" + String(QState7) + "Knop8:" + String(QState8));
+    }
+#pragma endregion
+
+// Test Seriële communicatie
+#pragma region teststring
+
+    if (incomingString == "Test")
+    {
+        Serial1.println("test geslaagd");
+        Serial.println("Test geslaagd");
+    }
 }
+#pragma endregion
